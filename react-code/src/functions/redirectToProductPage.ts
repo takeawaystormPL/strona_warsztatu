c37c01979b8c604286e59b47c99f6c35078ddb04
takeawaystormPL <151251780+takeawaystormPL@users.noreply.github.com>
2026-02-18 12:00:05 +0100

    18.02.2026

--- react-code/src/functions/redirectToProductPage.ts
+++
@@ -1,6 +0,0 @@
-export default function redirectToProductPage(productID: number) {
-    const pageURL = new URL(document.URL);
-    pageURL.pathname = "openedProduct";
-    pageURL.searchParams.set("id", String(productID));
-    return (window.location.href = pageURL.toString());
-  }
